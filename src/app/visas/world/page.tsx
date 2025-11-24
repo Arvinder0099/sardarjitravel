ae47f1253fbaabaf08d37f7f7896b6ff30c6fd60
Arvinder0099 <arvindersaini2523@gmail.com>
2025-11-24 15:13:42 +0400

    CRITICAL: Fix escaped backslashes in ALL import paths - now use proper relative paths

--- src/app/visas/world/page.tsx
+++ src/app/visas/world/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import Hero from "..\..\/components/Hero";
-import { WORLDWIDE_COUNTRIES } from "..\..\/../lib/worldwide";
+import Hero from "..\../components/Hero";
+import { WORLDWIDE_COUNTRIES } from "..\../../lib/worldwide";
 
 export const dynamic = "force-dynamic";
 
