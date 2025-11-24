9a68a738138eb7546ea1e79134e4b88b14aeb978
Arvinder0099 <arvindersaini2523@gmail.com>
2025-11-24 13:55:41 +0400

    SOLUTION: Remove admin pages - deploy public site only

--- src/app/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-export const runtime = "nodejs";
-
-import Dashboard from "../(components)/Dashboard";
-
-export default function AdminHome() {
-  return <Dashboard />;
-}
